07b9ef06ddb56efb1de6f32e1063721f4ca53bf4
Ramon Morales <rayman_mora70@yahoo.com>
2022-08-12 21:06:02 -0500

    Change grid logic

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+let container = document.getElementById("container");
 
-
-function grid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-function makeRows(rowNum) {
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-
-};
-
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
+function makeGrid(x) {
+    for (let r = 0; r < x; r++) {
+        let rows = document.createElement("div");
+        rows.className = "rows";
+        container.appendChild(rows);
+        for (let c = 0; c < x; c++) {
+            let grid = document.createElement("div");
+            grid.className = "grid";
+            container.appendChild(grid);
         };
-        
     };
-    
 };
 
-grid();
+makeGrid(16);

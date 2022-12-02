54748cab069961cf9a6980d04bc3b5269efaebfe
Ramon Morales <rayman_mora70@yahoo.com>
2022-12-01 19:41:53 -0600

    Change grid logic and add more elements

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
+const DEFAULT_SIZE = 16;
 let container = document.getElementById("container");
+const grid = document.getElementById("grid");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const clearBtn = document.getElementById("clearBtn");
+let currentSize = DEFAULT_SIZE;
 
-function makeGrid(x) {
-    for (let r = 0; r < x; r++) {
-        let rows = document.createElement("div");
-        rows.className = "rows";
-        container.appendChild(rows);
-        for (let c = 0; c < x; c++) {
-            let grid = document.createElement("div");
-            grid.className = "grid";
-            container.appendChild(grid);
-        };
-    };
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
+}
+
+function reloadGrid() {
+    clearGrid();
+    makeGrid(currentSize);
+}
+
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+}
+
+function makeGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("grid-element");
+        gridElement.addEventListener("mouseover", changeColor);
+        gridElement.addEventListener("mousedown", changeColor);
+        grid.appendChild(gridElement);
+    }
 };
 
-makeGrid(16);
+window.onload = () => {
+    makeGrid(DEFAULT_SIZE);
+}
